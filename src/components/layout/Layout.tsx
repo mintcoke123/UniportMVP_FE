c6618931036ff78b05d6e7a4c3c27fe6c399116c
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-02-28 06:24:14 +0900

    팀 참가방 overflow 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col h-dvh overflow-hidden">
       <Header />
-      <main className="flex-1 min-h-0 overflow-hidden flex flex-col pb-16 md:pb-0">
+      <main className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden flex flex-col pb-16 md:pb-0">
         <Outlet />
       </main>
       <BottomNav />
